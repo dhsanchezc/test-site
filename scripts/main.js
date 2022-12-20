8a8b792dcacfe82fbfc8bed06aed09cf5aa15c33
Diego <disan100@gmail.com>
2022-12-19 21:37:06 -0500

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,33 @@
+const myImage = document.querySelector("img");
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute("src");
+    if (mySrc === "images/car.png") {
+        myImage.setAttribute("src", "images/rotatedCar.png");
+    } else {
+        myImage.setAttribute("src", "images/car.png");
+    }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name");
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem("name", myName);
+        myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+}
+
+if (!localStorage.getItem("name")) {
+    setUserName();
+} {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+}
+
+myButton.onclick = () => {
+    setUserName();
+}
